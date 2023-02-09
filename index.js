744bd2f2c6202bbeefba0680c1d404e7fab5253a
hydramin <genetamin@gmail.com>
2023-02-09 11:01:03 -0500

    puts point, circle

--- index.js
+++ index.js
@@ -45,3 +45,54 @@ for(let i=0;i<=xmax;i+=unit) {
   vtick(i,ymax/2)
 }
 
+// given (0,0) means (xmax/2,ymax/2)
+// +x means adding to x + n.unit
+// -x means minusing x - n.unit
+// put a point (x,y)
+
+const origin = {x:xmax/2, y:ymax/2}
+
+function Point (x,y) {
+  this.x = origin.x + x * unit
+  this.y = origin.y - y * unit
+}
+
+function Circle (x,y,r) {
+  const pt = new Point(x,y)
+  this.x = pt.x
+  this.y = pt.y
+  this.r = scaleLen(r)
+}
+
+function scaleLen(ln) {
+  return ln * unit
+}
+
+function putPoint(x,y) {
+  ctx.beginPath()
+  const pt = new Point(x,y)
+  ctx.strokeStyle = "red"
+  ctx.arc(pt.x,pt.y,1,0,2*Math.PI)
+  ctx.fill()
+  ctx.stroke()
+}
+
+putPoint(1,1)
+putPoint(-1,1)
+putPoint(1,-1)
+putPoint(-1,-1)
+putPoint(0,0)
+
+function drawCircle(x,y,r) {
+  ctx.beginPath()
+  const c = new Circle(x,y,r)
+  ctx.arc(c.x,c.y,c.r,0,2*Math.PI)
+  ctx.strokeWidth = 1
+  ctx.strokeStyle = "black"
+  ctx.stroke()
+}
+
+drawCircle(1,1,1)
+drawCircle(2,-1,2)
+
+
